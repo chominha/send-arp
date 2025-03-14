79cbcacb84255b6b9f6af9abbd5bc4ccd7f621ca
gilgil <gilgil@gilgil.net>
2025-03-14 20:16:06 +0900

    Change all ip and mac value into zero

--- src/main.cpp
+++ src/main.cpp
@@ -31,19 +31,19 @@ int main(int argc, char* argv[]) {
 
 	EthArpPacket packet;
 
-	packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");
-	packet.eth_.smac_ = Mac("b4:2e:99:ea:97:de");
+	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
+	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
 	packet.eth_.type_ = htons(EthHdr::Arp);
 
 	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
 	packet.arp_.pro_ = htons(EthHdr::Ip4);
 	packet.arp_.hln_ = Mac::SIZE;
 	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:11:22:33:44:55");
-	packet.arp_.sip_ = htonl(Ip("10.1.1.1"));
+
+	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
+	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
 	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip("10.1.1.1"));
+	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
 
 	int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
